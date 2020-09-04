0c269d6ac317cdf4527bdfde26edc0d6a4c8ad19
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-04 05:59:25 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   
   def self.create
     song = self.new
-    @@all << song
+    song.save
     song
   end
   
@@ -31,7 +31,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    @@all.find{|song| song.name == name}
+    self.all.find{|song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
@@ -39,20 +39,30 @@ class Song
   end
  
  def self.alphabetical
-  all.sort_by{ |word| word.name }
+  all.sort_by{ |song| song.name }
  end
  
  def self.new_from_filename(name)
     song = self.new 
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+    split_name = name.chomp(".mp3").split(" - ")
+    song.name = split_name[1]
+    song.artist_name = split_name[0]
+    
+    # song.name = (name.split(" - ")[1].chomp(".mp3"))
+    # song.artist_name = (name.split(" - ")[0])
+    
     song
   end
   
  def self.create_from_filename(name)
-   song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+    song = self.new
+     split_name = name.chomp(".mp3").split(" - ")
+    song.name = split_name[1]
+    song.artist_name = split_name[0]
+    
+    # song.name = (name.split(" - ")[1].chomp(".mp3"))
+    # song.artist_name = (name.split(" - ")[0])
+    
     @@all << song
     song
   end
